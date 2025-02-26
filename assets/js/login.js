af51762f1e705e21add37fb78f8d6d012fa60765
FelipeYusti <felipe_yus@outlook.com>
2025-02-26 17:48:06 -0500

    Validacion de Login y registro de usuario funcionado

---
+++ assets/js/login.js
@@ -0,0 +1,98 @@
+let userName = document.querySelector("#userName");
+let password = document.querySelector("#userPass");
+let regUserName = document.querySelector("#regUserName"); // reg: register, se le asigna para diferenciar de los input de login
+let regPassword = document.querySelector("#regUserPass");
+let email = document.querySelector("#regUserEmail");
+let pais = document.querySelector("#regPaisUser");
+let frmLogin = document.querySelector("#frmLogin");
+let frmRegister = document.querySelector("#frmRegister");
+const api = "http://127.0.0.1:4000/API/user/";
+frmLogin.addEventListener("submit", (e) => {
+  e.preventDefault(); // previene el evento por defecto de los formularios
+  let accion = e.target.closest("form").getAttribute("data-tipo");
+  if (accion === "login") {
+    fetch(api + "loginUser", {
+      method: "POST",
+      // configuramos la cabecera, Header de peticion lleva una configuracin : contiene un archivo JS a JSON
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        userName: userName.value,
+        password: password.value
+      })
+    })
+      .then((res) => res.json())
+      .then((res) => {
+        if (res) {
+          window.location.href = "index.html";
+        } else {
+          Swal.fire({
+            title: "Opss..",
+            icon: "info",
+            text: res.mensaje
+          });
+        }
+      });
+  }
+});
+frmRegister.addEventListener("submit", (e) => {
+  e.preventDefault();
+  let accion = e.target.closest("form").getAttribute("data-tipo");
+
+  if (accion === "register") {
+    if (validarInputs(regUserName.value, email.value, regPassword.value, pais)) {
+      fetch(api + "newUser", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+          userName: regUserName.value.trim(),
+          email: email.value.trim(),
+          password: regPassword.value,
+          pais: pais.value
+        })
+      })
+        .then((res) => res.json())
+        .then((res) => {
+          if (res) {
+            regUserName.value = "";
+            email.value = "";
+            regPassword.value = "";
+            pais.value = "";
+            Swal.fire({
+              position: "top",
+              title: "Felicitaciones!",
+              icon: "success",
+              text: res.mensaje,
+              showConfirmButton: false,
+              timer: 1500
+            });
+          } else {
+            Swal.fire({
+              title: "Error!",
+              icon: "error",
+              text: res.mensaje
+            });
+          }
+        });
+    } else {
+      Swal.fire({
+        title: "Error!",
+        icon: "error",
+        text: "No pueden haber campos vacios"
+      });
+    }
+  }
+});
+
+function validarInputs(user, email, password, pais) {
+  if (!user && !email && !password && !pais) {
+    if (user.length >= 4) {
+      return true;
+    }
+  } else {
+    return false;
+  }
+}
