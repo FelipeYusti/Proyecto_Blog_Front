84ef8bbf444982f01dab31a744286a1fa206453c
Felipe Yusti <felipe_yus@outlook.com>
2025-03-04 11:44:12 -0500

    ahi vamos

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,7 +1,382 @@
 document.addEventListener("DOMContentLoaded", () => {
-  if (!sessionStorage.getItem("user")) {
+  if (!sessionStorage.getItem("userName")) {
     window.location.href = "login.html";
   } else {
-      alert("Bienvenido a mi mi blog")
+    const sesion = JSON.parse(sessionStorage.getItem("userName"));
+    nameUser.innerHTML = sesion.user;
+    const searchInput = document.getElementById("search-posts");
+    const postsList = document.querySelector(".posts-list");
+
+    // Datos de posts simulados
+    let posts = [
+      {
+        _id: "1",
+        autor_id: "1",
+        titulo: "El futuro del fútbol tras la Copa del Mundo",
+        sub_titulo:
+          "Análisis de las nuevas generaciones y su impacto en el fútbol mundial",
+        contenido_publicacion:
+          "El fútbol continúa evolucionando a medida que nuevas generaciones de jugadores irrumpen en la escena internacional. Equipos de todo el mundo buscan adaptarse a las nuevas tácticas y formas de juego...",
+        categoria: "Fútbol",
+        rutImagen:
+          "https://static.futbolfantasy.com/uploads/images/fotos_noticias1920/20231001-realmadrid.jpg",
+        fecha_publicacion: "2025-03-04",
+      },
+      {
+        _id: "2",
+        autor_id: "2",
+        titulo: "Grand Slam: El dominio de los grandes tenistas",
+        sub_titulo:
+          "Roger Federer, Rafael Nadal y Novak Djokovic siguen liderando el tenis mundial",
+        contenido_publicacion:
+          "A pesar de los años, los tres grandes del tenis continúan dominando los torneos más importantes del circuito. Sus enfrentamientos en los Grand Slams se han convertido en un espectáculo imperdible...",
+        categoria: "Tenis",
+        rutImagen:
+          "https://static.futbolfantasy.com/uploads/images/fotos_noticias1920/20231001-realmadrid.jpg",
+        fecha_publicacion: "2025-03-04",
+      },
+      {
+        _id: "5",
+        autor_id: "3",
+        titulo: "La NBA se prepara para los playoffs",
+        sub_titulo:
+          "Equipos como los Lakers y los Bucks están listos para la fase decisiva de la temporada",
+        contenido_publicacion:
+          "Con la temporada regular de la NBA llegando a su fin, los equipos de élite están afinando sus estrategias para los playoffs. Los Lakers, liderados por LeBron James, se perfilan como favoritos para el título...",
+        categoria: "Baloncesto",
+        rutImagen:
+          "https://static.futbolfantasy.com/uploads/images/fotos_noticias1920/20231001-realmadrid.jpg",
+        fecha_publicacion: "2025-03-04",
+      },
+      {
+        _id: "7",
+        autor_id: "4",
+        titulo: "Atletismo: El nuevo récord mundial de 100 metros",
+        sub_titulo:
+          "Un joven velocista logra una marca histórica en la disciplina",
+        contenido_publicacion:
+          "El joven corredor ha sorprendido al mundo con su tiempo récord en los 100 metros. Con una impresionante marca de 9.56 segundos, ha dejado atrás a algunos de los mejores atletas de todos los tiempos...",
+        categoria: "Atletismo",
+        rutImagen:
+          "https://static.futbolfantasy.com/uploads/images/fotos_noticias1920/20231001-realmadrid.jpg",
+        fecha_publicacion: "2025-03-04",
+      },
+      {
+        _id: "8",
+        autor_id: "5",
+        titulo: "Los secretos de la selección nacional de fútbol",
+        sub_titulo:
+          "Una mirada detrás de los entrenamientos y la mentalidad ganadora",
+        contenido_publicacion:
+          "El equipo nacional ha dado un paso más en su camino hacia la gloria. A través de entrenamientos rigurosos y estrategias innovadoras, están preparados para enfrentarse a los equipos más poderosos del mundo...",
+        categoria: "Fútbol",
+        rutImagen:
+          "https://static.futbolfantasy.com/uploads/images/fotos_noticias1920/20231001-realmadrid.jpg",
+        fecha_publicacion: "2025-03-05",
+      },
+      {
+        _id: "9",
+        autor_id: "6",
+        titulo: "El auge de los tenistas jóvenes en el circuito ATP",
+        sub_titulo: "Nuevas estrellas emergen y desafían a los veteranos",
+        contenido_publicacion:
+          "La juventud está dominando el circuito ATP, con nuevos jugadores que están haciendo historia con su rapidez y habilidad. Conozca a los tenistas que están remodelando el tenis...",
+        categoria: "Tenis",
+        rutImagen:
+          "https://static.futbolfantasy.com/uploads/images/fotos_noticias1920/20231001-realmadrid.jpg",
+        fecha_publicacion: "2025-03-06",
+      },
+      {
+        _id: "11",
+        autor_id: "7",
+        titulo: "La evolución de los uniformes en la NBA",
+        sub_titulo:
+          "Desde los años 80 hasta la actualidad, el diseño ha cambiado radicalmente",
+        contenido_publicacion:
+          "La vestimenta de los jugadores de baloncesto no solo ha sido una cuestión de estilo, sino también de funcionalidad. El uso de materiales avanzados ha ayudado a mejorar el rendimiento en la cancha...",
+        categoria: "Baloncesto",
+        rutImagen:
+          "https://static.futbolfantasy.com/uploads/images/fotos_noticias1920/20231001-realmadrid.jpg",
+        fecha_publicacion: "2025-03-07",
+      },
+      {
+        _id: "10",
+        autor_id: "8",
+        titulo: "Atletismo: La carrera por los Juegos Olímpicos de París 2024",
+        sub_titulo:
+          "Los mejores atletas luchan por un puesto en la competencia mundial",
+        contenido_publicacion:
+          "Con los Juegos Olímpicos de París a la vuelta de la esquina, los atletas de todo el mundo están intensificando su preparación para asegurar un lugar en la competencia más grande del planeta...",
+        categoria: "Atletismo",
+        rutImagen:
+          "https://static.futbolfantasy.com/uploads/images/fotos_noticias1920/20231001-realmadrid.jpg",
+        fecha_publicacion: "2025-03-08",
+      },
+      {
+        _id: "14",
+        autor_id: "9",
+        titulo: "El impacto del VAR en el fútbol moderno",
+        sub_titulo:
+          "Un análisis del uso del video arbitraje en las competiciones internacionales",
+        contenido_publicacion:
+          "El VAR ha cambiado la forma en que se toman las decisiones en el fútbol. Con su implementación, los árbitros tienen la capacidad de revisar jugadas polémicas, lo que ha generado debate entre jugadores, entrenadores y fanáticos...",
+        categoria: "Fútbol",
+        rutImagen:
+          "https://static.futbolfantasy.com/uploads/images/fotos_noticias1920/20231001-realmadrid.jpg",
+        fecha_publicacion: "2025-03-09",
+      },
+      {
+        _id: "14",
+        autor_id: "10",
+        titulo: "Los grandes duelos del tenis femenino",
+        sub_titulo:
+          "Grandes rivalidades como Serena Williams vs. Venus Williams",
+        contenido_publicacion:
+          "El tenis femenino ha visto algunas de las rivalidades más intensas de todos los tiempos. La competencia entre jugadoras ha llevado el tenis a nuevos niveles de emoción y drama...",
+        categoria: "Tenis",
+
+        rutImagen:
+          "https://static.futbolfantasy.com/uploads/images/fotos_noticias1920/20231001-realmadrid.jpg",
+        fecha_publicacion: "2025-03-10",
+      },
+      {
+        _id: "14",
+        autor_id: "11",
+        titulo: "La historia de los Chicago Bulls en los 90",
+        sub_titulo:
+          "Un recorrido por la dinastía más dominante en la historia de la NBA",
+        contenido_publicacion:
+          "Los Chicago Bulls, bajo la dirección de Michael Jordan, dominaron la NBA durante los años 90. Con seis campeonatos en ocho años, su legado sigue vivo en los corazones de los fanáticos...",
+        categoria: "Baloncesto",
+        rutImagen:
+          "https://static.futbolfantasy.com/uploads/images/fotos_noticias1920/20231001-realmadrid.jpg",
+        fecha_publicacion: "2025-03-11",
+      },
+      {
+        _id: "14",
+        autor_id: "12",
+        titulo: "El atletismo y la tecnología: un futuro prometedor",
+        sub_titulo:
+          "Cómo la tecnología está transformando el entrenamiento y rendimiento de los atletas",
+        contenido_publicacion:
+          "La innovación tecnológica está cambiando la manera en que los atletas entrenan, desde el uso de sensores en las zapatillas hasta los avances en el análisis de datos para mejorar el rendimiento...",
+        categoria: "Atletismo",
+        rutImagen:
+          "https://static.futbolfantasy.com/uploads/images/fotos_noticias1920/20231001-realmadrid.jpg",
+        fecha_publicacion: "2025-03-12",
+      },
+      {
+        _id: "14",
+        autor_id: "13",
+        titulo: "Fútbol femenino: un deporte en crecimiento",
+        sub_titulo:
+          "El aumento de la visibilidad y popularidad del fútbol femenino en todo el mundo",
+        contenido_publicacion:
+          "El fútbol femenino ha experimentado un auge en popularidad en los últimos años. Cada vez son más las ligas y equipos que invierten en el desarrollo de este deporte a nivel mundial...",
+        categoria: "Fútbol",
+        rutImagen:
+          "https://static.futbolfantasy.com/uploads/images/fotos_noticias1920/20231001-realmadrid.jpg",
+        fecha_publicacion: "2025-03-13",
+      },
+      {
+        _id: "14",
+        autor_id: "14",
+        titulo: "El regreso de los grandes tenistas tras las lesiones",
+        sub_titulo:
+          "Nadal, Federer y Djokovic vuelven a las canchas después de sus lesiones más graves",
+        contenido_publicacion:
+          "Los tres grandes del tenis han superado lesiones que amenazaron con poner fin a sus carreras. Hoy en día, siguen siendo figuras importantes en el circuito ATP...",
+        categoria: "Tenis",
+        rutImagen:
+          "https://static.futbolfantasy.com/uploads/images/fotos_noticias1920/20231001-realmadrid.jpg",
+        fecha_publicacion: "2025-03-14",
+      },
+      {
+        _id: "14",
+        autor_id: "15",
+        titulo: "La magia de Stephen Curry: El mejor tirador de la historia",
+        sub_titulo:
+          "Stephen Curry sigue rompiendo récords en la NBA con su increíble capacidad de tiro",
+        contenido_publicacion:
+          "Stephen Curry ha revolucionado el baloncesto con su habilidad para lanzar triples desde cualquier punto del campo. Con cada temporada, su leyenda crece y sigue dejando su huella en la NBA...",
+        categoria: "Baloncesto",
+        rutImagen:
+          "https://static.futbolfantasy.com/uploads/images/fotos_noticias1920/20231001-realmadrid.jpg",
+        fecha_publicacion: "2025-03-15",
+      },
+    ];
+
+    console.log(posts);
+    // Renderiza los posts en la interfaz
+    function displayPosts(filteredPosts) {
+      postsList.innerHTML = "";
+      filteredPosts.forEach((post) => {
+        const postCard = document.createElement("div");
+        const col = document.createElement("div");
+        col.setAttribute("class", "col-md-10");
+        postCard.classList.add("post-card");
+        postCard.setAttribute("class", "article card mb-4 shadow-sm");
+        postCard.innerHTML = `
+         <!-- Imagen del post -->
+              <img
+                src="${post.rutImagen}"
+                class="card-img-top"
+                alt="Imagen del post"
+                id="imagenPost"
+                onerror="this.style.display='block'"
+              />
+  
+              <div class="card-body">
+                <h3 class="card-title">
+                ${post.titulo}
+                </h3>
+                <p class="text-muted">
+                  ${post.fecha_publicacion} por 
+                </p>
+                <p id="summary" class="card-text">
+                ${post.sub_titulo}
+                  
+                </p>
+                <p id="full-content" class="hidden-content">
+                  ${post.contenido_publicacion}
+                </p>
+                <button class="btn btn-primary" id="read-more-btn">
+                  Leer más
+                </button>
+              </div>
+  
+              <!-- Sección de Comentarios -->
+              <div class="card-footer">
+                <h5>Comentarios</h5>
+                <button
+                  class="btn btn-outline-dark"
+                  id="toggle-comments-btn"
+                >
+                  Mostrar comentarios
+                </button>
+                <div
+                  class="comment-list mt-3"
+                  id="comment-list"
+                  style="display: none"
+                >
+                  <div class="comment-item">
+                    <strong>María Gómez:</strong>
+                    <p>
+                      ¡Gran artículo! Los consejos son muy útiles,
+                      especialmente para mejorar la agilidad.
+                    </p>
+                  </div>
+                  <div class="comment-item">
+                    <strong>Carlos Martínez:</strong>
+                    <p>
+                      ¡Interesante enfoque! Definitivamente probaré estos
+                      ejercicios.
+                    </p>
+                  </div>
+                </div>
+                <div class="comment-input mt-3">
+                  <form>
+                    <textarea
+                      class="form-control"
+                      id="commentText"
+                      rows="3"
+                      placeholder="Escribe tu comentario aquí..."
+                    ></textarea>
+                    <button type="submit" class="btn btn-success mt-2">
+                      Enviar Comentario
+                    </button>
+                  </form>
+                </div>
+                <div
+                    class="post-actions position-absolute rounded-pill top-0 end-0 m-1"
+                  >
+                    <button
+                      class="btn btn-light btn-actions rounded-pill edit-button me-1" data-bs-toggle="modal" data-bs-target="#addPostModal"
+                      data-id="${post._id}"
+                    >
+                      <i
+                        class="bi bi-pencil-square text-dark"
+                        style="font-size: larger"
+                      ></i>
+                    </button>
+                    <button
+                      class="btn btn-danger btn-actions rounded-pill delete-button"
+                      data-id="${post._id}"
+                    >
+                      <i
+                        class="bi bi-trash3 text-dark"
+                        style="font-size: larger"
+                      ></i>
+                    </button>
+                  </div>
+          </div>
+      `;
+
+        col.appendChild(postCard);
+        postsList.appendChild(col);
+      });
+    }
+
+    // Filtros de búsqueda
+    searchInput.addEventListener("input", function () {
+      const query = searchInput.value.toLowerCase();
+      const filteredPosts = posts.filter((post) =>
+        post.title.toLowerCase().includes(query)
+      );
+      displayPosts(filteredPosts);
+    });
+
+    // Eventos para los botones de acción
+    // Ordenar por título
+    document
+      .getElementById("filter-title")
+      .addEventListener("click", function () {
+        posts.sort((a, b) => a.title.localeCompare(b.title));
+        displayPosts(posts);
+      });
+
+    // Ordenar por fecha (simulado, en este caso sin cambios)
+    document
+      .getElementById("filter-date")
+      .addEventListener("click", function () {
+        alert("Ordenar por fecha aún no implementado.");
+      });
+
+    // Inicializar la visualización
+    displayPosts(posts);
   }
+  // DESPLIEGA LOS COMENTARIOS
+  document
+    .getElementById("toggle-comments-btn")
+    .addEventListener("click", (e) => {
+      e.preventDefault();
+      let commentList = document.getElementById("comment-list");
+      if (commentList.style.display === "none") {
+        commentList.style.display = "block";
+        this.textContent = "Ocultar comentarios";
+      } else {
+        commentList.style.display = "none";
+        this.textContent = "Mostrar comentarios";
+      }
+    });
+  //  "Leer más" : Despliega el resto del contenido
+  document
+    .getElementById("read-more-btn")
+    .addEventListener("click", function () {
+      let fullContent = document.getElementById("full-content");
+      let summary = document.getElementById("summary");
+
+      if (
+        fullContent.style.display === "none" ||
+        fullContent.style.display === ""
+      ) {
+        fullContent.style.display = "block";
+        summary.style.display = "none";
+        this.textContent = "Leer menos";
+      } else {
+        fullContent.style.display = "none";
+        summary.style.display = "block";
+        this.textContent = "Leer más";
+      }
+    });
 });
--- assets/js/usuario.js
+++ assets/js/usuario.js
@@ -2,6 +2,8 @@ const api = "http://127.0.0.1:4000/api/publicaciones/"; // nos conectamos a la A
 const sesion = JSON.parse(sessionStorage.getItem("userName"));
 let nameUser = document.querySelector("#nameUser");
 let btnClaseSesion = document.querySelector("#btnCerrarSesion");
+let btnGuardar = document.querySelector("#btnCerrarSesion");
+
 // cerrar sesion
 btnClaseSesion.addEventListener("click", (e) => {
   if (!localStorage.getItem("userName")) {
@@ -10,15 +12,15 @@ btnClaseSesion.addEventListener("click", (e) => {
     history.replaceState(null, null, window.location.href);
   }
 });
-nameUser.innerHTML = sesion.user;
+
 // FORMULARIO DE LA PUBLICACION O POST
-document.getElementById("frmPost").addEventListener("submit", function (event) {
+ document.getElementById("frmPost").addEventListener("submit", (event) => {
   event.preventDefault();
   const fecha = new Date();
   const options = {
     year: "numeric", // Año
     month: "long", // Mes
-    day: "numeric" // Dia
+    day: "numeric", // Dia
   };
   const fechaPublicacion = fecha.toLocaleDateString(undefined, options);
   const titulo = document.querySelector("#postTitle");
@@ -29,7 +31,7 @@ document.getElementById("frmPost").addEventListener("submit", function (event) {
   fetch(api + "nuevaPublicacion", {
     method: "POST",
     headers: {
-      "Content-Type": "application/json"
+      "Content-Type": "application/json",
     },
     body: JSON.stringify({
       autor_id: sesion.user_id,
@@ -37,8 +39,8 @@ document.getElementById("frmPost").addEventListener("submit", function (event) {
       rutImagen: image.value,
       categoria: categories.value,
       contenido_publicacion: contentenido.value,
-      fecha_publicacion: fechaPublicacion
-    })
+      fecha_publicacion: fechaPublicacion,
+    }),
   })
     .then((res) => res.json())
     .then((res) => {
@@ -49,13 +51,13 @@ document.getElementById("frmPost").addEventListener("submit", function (event) {
           icon: "success",
           text: res.mensaje,
           showConfirmButton: false,
-          timer: 1500
+          timer: 1500,
         });
       } else {
         Swal.fire({
           title: "Error!",
           icon: "error",
-          text: res.mensaje
+          text: res.mensaje,
         });
       }
     })
@@ -63,73 +65,34 @@ document.getElementById("frmPost").addEventListener("submit", function (event) {
       Swal.fire({
         title: "El servidor no responde!",
         icon: "error",
-        text: error
+        text: error,
       });
     });
 
   // document.getElementById("frmPost").reset();
   bootstrap.Modal.getInstance(document.getElementById("addPostModal")).hide();
 });
-// Mostrar/Ocultar comentarios
-document.getElementById("toggle-comments-btn").addEventListener("click", function () {
-  let commentList = document.getElementById("comment-list");
-  if (commentList.style.display === "none") {
-    commentList.style.display = "block";
-    this.textContent = "Ocultar comentarios";
-  } else {
-    commentList.style.display = "none";
-    this.textContent = "Mostrar comentarios";
-  }
-});
+/* function attachEventListeners() {
+  document.querySelectorAll(".view-button").forEach((button) => {
+    button.addEventListener("click", function () {
+      alert(`Ver post ${this.dataset.id}`);
+    });
+  });
 
-// Funcionalidad del botón "Leer más"
-document.getElementById("read-more-btn").addEventListener("click", function () {
-  let fullContent = document.getElementById("full-content");
-  let summary = document.getElementById("summary");
+  document.querySelectorAll(".edit-button").forEach((button) => {
+    button.addEventListener("click", function () {
+      alert(`Editar post ${this.dataset.id}`);
+    });
+  });
 
-  if (fullContent.style.display === "none" || fullContent.style.display === "") {
-    fullContent.style.display = "block";
-    summary.style.display = "none";
-    this.textContent = "Leer menos";
-  } else {
-    fullContent.style.display = "none";
-    summary.style.display = "block";
-    this.textContent = "Leer más";
-  }
-});
-/**
- Tenis: "Las Nuevas Estrellas del Tenis: ¿Quién Tomará el Mando Después de Federer, Nadal y Djokovic?"
-El tenis ha vivido una era dorada con grandes leyendas como Roger Federer, Rafael Nadal y Novak Djokovic dominando los Grand Slams durante más de una década. Sin embargo, la pregunta que muchos se hacen es: ¿quién tomará el control del circuito una vez que estos gigantes se retiren?
+  document.querySelectorAll(".delete-button").forEach((button) => {
+    button.addEventListener("click", function () {
+      if (confirm("¿Seguro que deseas eliminar este post?")) {
+        posts = posts.filter((post) => post.id != this.dataset.id);
+        displayPosts(posts);
+      }
+    });
+  });
+} */
 
-Jóvenes promesas como Carlos Alcaraz y Jannik Sinner ya están demostrando su talento en las pistas, mientras otros como Daniil Medvedev y Alexander Zverev siguen buscando consolidarse. Analizamos el panorama actual del tenis, las características de estos nuevos jugadores y las perspectivas para el futuro del deporte.
 
-Atletismo: "Las Grandes Promesas del Atletismo: ¿Quién Será el Próximo Usain Bolt?"
-El atletismo es un deporte que ha tenido momentos épicos, como la carrera de Usain Bolt en los 100 metros, pero, ¿quién tomará su lugar como la próxima gran figura? Los velocistas jóvenes de todo el mundo están empujando los límites y desafiando los récords.
-
-Exploramos a las nuevas estrellas que podrían reinar en las pistas, como el canadiense Andre De Grasse, el estadounidense Noah Lyles o el jamaiquino Oblique Seville. Además, abordamos la importancia de la tecnología en el atletismo moderno, cómo el entrenamiento se ha optimizado para obtener resultados y cómo se están cambiando los estándares de velocidad.
-
-Baloncesto: "NBA: La Rivalidad de Hoy, ¿Quién Dominará la Liga en la Próxima Década?"
-El baloncesto es un deporte lleno de rivalidades intensas, y la NBA no es la excepción. Con leyendas como LeBron James, Stephen Curry y Kevin Durant aún siendo figuras centrales, los ojos del mundo están puestos en quién será el próximo gran ícono que llevará la liga a nuevas alturas.
-
-Jugadores como Luka Dončić, Giannis Antetokounmpo y Zion Williamson ya están demostrando su potencial para ser los futuros pilares del baloncesto. Este artículo analiza las nuevas estrellas que se están levantando y cómo el juego está evolucionando, desde la importancia de la versatilidad en los jugadores hasta el impacto de la revolución de los triples en la NBA.
-
-Fútbol: "La Influencia de los Entrenadores Modernos: Cómo Han Cambiado la Filosofía del Fútbol"
-El fútbol no solo se juega en el campo, sino también en las mentes de los entrenadores. Entrenadores como Pep Guardiola, Jürgen Klopp y Diego Simeone han cambiado la forma en que entendemos el fútbol. Analizamos cómo sus filosofías han transformado el juego, desde el estilo de posesión de Guardiola hasta el contragolpe letal de Simeone.
-
-También abordamos la influencia de los entrenadores en la mentalidad de los jugadores y cómo un buen técnico puede llevar a un equipo a niveles de rendimiento que antes parecían imposibles.
-
-Tenis: "Cómo Prepararse Mentalmente para un Gran Torneo de Tenis"
-El tenis no solo es físico; también es un juego mental. La capacidad de mantener la calma, la concentración y la resiliencia bajo presión puede ser la diferencia entre ganar y perder en los momentos más importantes. En este post, exploramos estrategias psicológicas que los tenistas profesionales usan para mantenerse a la vanguardia durante los partidos cruciales.
-
-Hablamos sobre la importancia de la visualización, cómo lidiar con los nervios y qué técnicas los jugadores utilizan para mantener la concentración cuando más lo necesitan, basándonos en los métodos de algunos de los mejores tenistas de la historia.
-
-Atletismo: "Los Mejores Consejos para Mejorar tu Rendimiento en los 400 Metros"
-Correr los 400 metros es un desafío que combina velocidad y resistencia. Muchos corredores lo consideran una de las pruebas más difíciles del atletismo, ya que requiere un equilibrio entre explosividad y resistencia. En este post, te daremos algunos consejos esenciales para mejorar tu rendimiento en los 400 metros, desde la técnica adecuada hasta la forma de manejar la fatiga y la velocidad.
-
-También analizamos las claves de la preparación mental para enfrentar una carrera tan exigente y cómo ajustar tu entrenamiento para alcanzar tu máximo potencial en esta distancia.
-
-Baloncesto: "Cómo la Evolución del Juego ha Influido en los Estilos de los Jugadores de la NBA"
-El baloncesto ha evolucionado a lo largo de los años, y esto se refleja en los jugadores. Desde la era de los pívots dominantes como Shaquille O'Neal hasta la era moderna de los jugadores versátiles que pueden hacer de todo, el juego ha cambiado radicalmente.
-
-Este artículo examina cómo las nuevas tendencias del baloncesto han influido en los estilos de juego de las estrellas actuales, desde la importancia de la defensa en el perímetro hasta la necesidad de una mayor habilidad en los tiros de tres puntos. También exploramos cómo las estadísticas avanzadas están redefiniendo lo que significa ser un jugador dominante en la NBA.
- */
