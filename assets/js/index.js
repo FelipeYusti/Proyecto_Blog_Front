44fdf1947bf10b2d285418e477ba34823d1e4ddb
FelipeYusti <felipe_yus@outlook.com>
2025-03-10 16:18:48 -0500

    vamos pastores vmaos

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,11 +3,14 @@ document.addEventListener("DOMContentLoaded", () => {
     window.location.href = "login.html";
   } else {
     const api = "http://127.0.0.1:4000/api/publicaciones/"; // nos conectamos a la API
+    const readImage = "http://127.0.0.1:4000/api/publicaciones/image/"; // nos conectamos al servidor para leer la imagen
     const sesion = JSON.parse(sessionStorage.getItem("userName"));
     nameUser.innerHTML = sesion.user;
-    const searchInput = document.getElementById("search-posts");
+    const searchInput = document.querySelector("#search-posts");
+    const mispost = document.querySelector("#misPost");
     const postsList = document.querySelector(".posts-list");
     let post = [];
+
     // Datos de posts simulados
     fetch(api + "listarPorIdPublicacion/" + sesion.user_id, {
       method: "GET"
@@ -24,8 +27,9 @@ document.addEventListener("DOMContentLoaded", () => {
         console.log(error);
       });
     // Renderiza los posts en la interfaz
+
     function displayPosts(filteredPosts) {
-      postsList.innerHTML = "";
+      //postsList.innerHTML = "";
       let indice = 0;
 
       filteredPosts.forEach((post) => {
@@ -35,101 +39,101 @@ document.addEventListener("DOMContentLoaded", () => {
         postCard.classList.add("post-card");
         postCard.setAttribute("class", "article card mb-4 shadow-sm");
         postCard.innerHTML = `
-         <!-- Imagen del post -->
-              <img
-                src=""
-                class="card-img-top"
-                alt="Imagen del post"
-                id="imagenPost"
-                onerror="this.style.display='block'"
-              />
-  
-              <div class="card-body">
-                <h3 class="card-title">
-                ${post.titulo}
-                </h3>
-                <p class="text-muted">
-                  ${post.fecha_publicacion} por 
-                </p>
-                <p id="summary" class="card-text">
-                ${post.sub_titulo}
-                  
-                </p>
-                <p id="full-content" class="hidden-content">
-                  ${post.contenido_publicacion}
-                </p>
-                <button class="btn btn-primary" id="read-more-btn">
-                  Leer más
-                </button>
-              </div>
-  
-              <!-- Sección de Comentarios -->
-              <div class="card-footer">
-                <h5>Comentarios</h5>
-                <button
-                  class="btn btn-outline-dark"
-                  id="toggle-comments-btn"
-                >
-                  Mostrar comentarios
-                </button>
-                <div
-                  class="comment-list mt-3"
-                  id="comment-list"
-                  style="display: none"
-                >
-                  <div class="comment-item">
-                    <strong>María Gómez:</strong>
-                    <p>
-                      ¡Gran artículo! Los consejos son muy útiles,
-                      especialmente para mejorar la agilidad.
-                    </p>
-                  </div>
-                  <div class="comment-item">
-                    <strong>Carlos Martínez:</strong>
-                    <p>
-                      ¡Interesante enfoque! Definitivamente probaré estos
-                      ejercicios.
-                    </p>
-                  </div>
-                </div>
-                <div class="comment-input mt-3">
-                  <form>
-                    <textarea
-                      class="form-control"
-                      id="commentText"
-                      rows="3"
-                      placeholder="Escribe tu comentario aquí..."
-                    ></textarea>
-                    <button type="submit" class="btn btn-success mt-2">
-                      Enviar Comentario
-                    </button>
-                  </form>
-                </div>
-                <div
-                    class="post-actions position-absolute rounded-pill top-0 end-0 m-1"
-                  >
-                    <button
-                      class="btn btn-light btn-actions rounded-pill edit-button me-1" data-bs-toggle="modal" data-bs-target="#addPostModal"
-                      data-id="${post._id}" data-action="edit"
-                    >
-                      <i
-                        class="bi bi-pencil-square text-dark"
-                        style="font-size: larger"
-                      ></i>
-                    </button>
-                    <button
-                      class="btn btn-danger btn-actions rounded-pill delete-button" id="delete-button" 
-                      data-id="${post._id}"
-                      data-action="delete"
-                    >
-                      <i
-                        class="bi bi-trash3 text-dark"
-                        style="font-size: larger"
-                      ></i>
-                    </button>
-                  </div>
+   <!-- Imagen del post -->
+        <img
+          src="${post.rutImagen ? readImage + "" + post.rutImagen : ""}"
+          class="card-img-top"
+          alt="Imagen del post"
+          id="imagenPost"
+          onerror="this.style.display='block'"
+        />
+
+        <div class="card-body">
+          <h3 class="card-title">
+          ${post.titulo}
+          </h3>
+          <p class="text-muted">
+            ${post.fecha_publicacion} por 
+          </p>
+          <p id="summary" class="card-text">
+          ${post.sub_titulo}
+            
+          </p>
+          <p id="full-content" class="hidden-content">
+            ${post.contenido_publicacion}
+          </p>
+          <button class="btn btn-primary" id="read-more-btn">
+            Leer más
+          </button>
+        </div>
+
+        <!-- Sección de Comentarios -->
+        <div class="card-footer">
+          <h5>Comentarios</h5>
+          <button
+            class="btn btn-outline-dark"
+            id="toggle-comments-btn"
+          >
+            Mostrar comentarios
+          </button>
+          <div
+            class="comment-list mt-3"
+            id="comment-list"
+            style="display: none"
+          >
+            <div class="comment-item">
+              <strong>María Gómez:</strong>
+              <p>
+                ¡Gran artículo! Los consejos son muy útiles,
+                especialmente para mejorar la agilidad.
+              </p>
+            </div>
+            <div class="comment-item">
+              <strong>Carlos Martínez:</strong>
+              <p>
+                ¡Interesante enfoque! Definitivamente probaré estos
+                ejercicios.
+              </p>
+            </div>
           </div>
-      `;
+          <div class="comment-input mt-3">
+            <form>
+              <textarea
+                class="form-control"
+                id="commentText"
+                rows="3"
+                placeholder="Escribe tu comentario aquí..."
+              ></textarea>
+              <button type="submit" class="btn btn-success mt-2">
+                Enviar Comentario
+              </button>
+            </form>
+          </div>
+          <div
+              class="post-actions position-absolute rounded-pill top-0 end-0 m-1"
+            >
+              <button
+                class="btn btn-light btn-actions rounded-pill edit-button me-1" data-bs-toggle="modal" data-bs-target="#addPostModal"
+                data-id="${post._id}" data-action="edit"
+              >
+                <i
+                  class="bi bi-pencil-square text-dark"
+                  style="font-size: larger"
+                ></i>
+              </button>
+              <button
+                class="btn btn-danger btn-actions rounded-pill delete-button" id="delete-button" 
+                data-id="${post._id}"
+                data-action="delete"
+              >
+                <i
+                  class="bi bi-trash3 text-dark"
+                  style="font-size: larger"
+                ></i>
+              </button>
+            </div>
+    </div>
+`;
 
         col.appendChild(postCard);
         postsList.appendChild(col);
--- assets/js/usuario.js
+++ assets/js/usuario.js
@@ -1,13 +1,15 @@
 const api = "http://127.0.0.1:4000/api/publicaciones/"; // nos conectamos a la API
+// nos conectamos a la API
 const sesion = JSON.parse(sessionStorage.getItem("userName"));
 let nameUser = document.querySelector("#nameUser");
 let btnClaseSesion = document.querySelector("#btnCerrarSesion");
 
+let autor_id = sesion.user_id;
 let titulo = document.querySelector("#postTitle");
 let sub_titulo = document.querySelector("#postSubTitle");
 let categories = document.querySelector("#categories");
-let contetenido = document.querySelector("#postContent");
 let image = document.querySelector("#postImage");
+let contenido = document.querySelector("#postContent");
 let idPost = 0;
 // cerrar sesion
 btnClaseSesion.addEventListener("click", (e) => {
@@ -22,65 +24,75 @@ document.addEventListener("click", (e) => {
   try {
     let action = e.target.closest("button").getAttribute("data-action");
     let id = e.target.closest("button").getAttribute("data-id");
-    if (action === "delete") {
-      Swal.fire({
-        title: "¿Estás seguro de esta accion?",
-        text: "La Publicacion se eliminara permanentemente. ",
-        icon: "warning",
-        showDenyButton: true,
-        confirmButtonText: "Confirmar",
-        denyButtonText: `Cancelar`
-      }).then((result) => {
-        /* Read more about isConfirmed, isDenied below */
-        if (result.isConfirmed) {
-          fetch(api + "borrarPorId/" + id, {
-            method: "DELETE"
-          })
-            .then((res) => res.json())
-            .then((res) => {
-              if (res.estado === true) {
+
+    switch (action) {
+      case "delete":
+        Swal.fire({
+          title: "¿Estás seguro de esta accion?",
+          text: "La Publicacion se eliminara permanentemente. ",
+          icon: "warning",
+          showDenyButton: true,
+          confirmButtonText: "Confirmar",
+          denyButtonText: `Cancelar`
+        }).then((result) => {
+          /* Read more about isConfirmed, isDenied below */
+          if (result.isConfirmed) {
+            fetch(api + "borrarPorId/" + id, {
+              method: "DELETE"
+            })
+              .then((res) => res.json())
+              .then((res) => {
+                if (res.estado === true) {
+                  Swal.fire({
+                    title: "Se Elimino el post correctamente.",
+                    icon: "success"
+                  });
+                  window.location.reload();
+                } else {
+                  Swal.fire({
+                    title: "No se pudo eliminar el post!",
+                    icon: "error",
+                    text: error
+                  });
+                }
+              })
+              .catch((error) => {
                 Swal.fire({
-                  title: "Se Elimino el post correctamente.",
-                  icon: "success"
-                });
-                window.location.reload();
-              } else {
-                Swal.fire({
-                  title: "No se pudo eliminar el post!",
+                  title: "El servidor no responde!",
                   icon: "error",
                   text: error
                 });
-              }
-            })
-            .catch((error) => {
-              Swal.fire({
-                title: "El servidor no responde!",
-                icon: "error",
-                text: error
               });
-            });
-        }
-      });
-    } else if (action === "edit") {
-      idPost = e.target.closest("button").getAttribute("data-id");
-      fetch(api + "listarPublicId/" + idPost, {
-        method: "GET"
-      })
-        .then((res) => res.json())
-        .then((res) => {
-          titulo.value = res.post.titulo;
-          sub_titulo.value = res.post.sub_titulo;
-          /* image.value = res.post.rutImagen;*/
-          categories.value = res.post.categoria;
-          contetenido.value = res.post.contenido_publicacion;
-        })
-        .catch((error) => {
-          Swal.fire({
-            title: "El servidor no responde!",
-            icon: "error",
-            text: error
-          });
+          }
         });
+        break;
+      case "edit":
+        idPost = e.target.closest("button").getAttribute("data-id");
+        fetch(api + "listarPublicId/" + idPost, {
+          method: "GET"
+        })
+          .then((res) => res.json())
+          .then((res) => {
+            titulo.value = res.post.titulo;
+            sub_titulo.value = res.post.sub_titulo;
+            categories.value = res.post.categoria;
+            contenido.value = res.post.contenido_publicacion;
+          })
+          .catch((error) => {
+            Swal.fire({
+              title: "El servidor no responde!",
+              icon: "error",
+              text: error
+            });
+          });
+        break;
+      default:
+        Swal.fire({
+          title: "!Accion no valida!",
+          icon: "error",
+          text: error
+        });
+        break;
     }
   } catch (error) {}
 });
@@ -89,50 +101,105 @@ document.addEventListener("click", (e) => {
 document.getElementById("frmPost").addEventListener("submit", (e) => {
   e.preventDefault();
   let action = document.querySelector("#btnGuardar").getAttribute("data-action");
+  const frmPost = new FormData(); 
+  const fecha = new Date();
+  const options = {
+    year: "numeric", // Año
+    month: "long", // Mes
+    day: "numeric" // Dia
+  };
+  const fechaPublicacion = fecha.toLocaleDateString(undefined, options);
 
-  if (action === "save-changes-post") {
-    fetch(api + "actualizarPorId/" + idPost, {
-      method: "PUT",
-      headers: {
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify({
-        titulo: titulo.value,
-        sub_titulo: sub_titulo.value,
-        /*  rutImagen: image.value, */
-        categoria: categories.value,
-        contenido_publicacion: contetenido.value
+  switch (action) {
+    case "public-post":
+      frmPost.append("autor_id", sesion.user_id);
+      frmPost.append("titulo", document.querySelector("#postTitle").value);
+      frmPost.append("sub_titulo", document.querySelector("#postSubTitle").value);
+      frmPost.append("rutImagen", document.querySelector("#postImage").files[0]); // Asegúrate de que image es un input de tipo "file"
+      frmPost.append("categoria", document.querySelector("#categories").value);
+      frmPost.append("contenido_publicacion", document.querySelector("#postContent").value);
+      frmPost.append("fecha_publicacion", fechaPublicacion);
+
+      fetch(api + "nuevaPublicacion/", {
+        method: "POST",
+        body: frmPost
       })
-    })
-      .then((res) => res.json())
-      .then((res) => {
-        console.log(res);
-        if (res.estado === true) {
+        .then((res) => res.json())
+        .then((res) => {
+          console.log(res);
+          if (res.estado === true) {
+            Swal.fire({
+              position: "top",
+              title: "Se publico el post!",
+              icon: "success",
+              text: res.mensaje,
+              showConfirmButton: false,
+              timer: 1500
+            });
+          } else {
+            Swal.fire({
+              title: "Error!",
+              icon: "error",
+              text: res.mensaje
+            });
+          }
+        })
+        .catch((error) => {
           Swal.fire({
-            position: "top",
-            title: "Se Modifico el post!",
-            icon: "success",
-            text: res.mensaje,
-            showConfirmButton: false,
-            timer: 1500
-          });
-        } else {
-          Swal.fire({
-            title: "Error!",
+            title: "El servidor no responde!",
             icon: "error",
-            text: res.mensaje
+            text: error
           });
-        }
-      })
-      .catch((error) => {
-        Swal.fire({
-          title: "El servidor no responde!",
-          icon: "error",
-          text: error
         });
-      });
-  }
+      break;
+    case "save-changes-post":
+      frmPost.append("titulo", document.querySelector("#postTitle").value);
+      frmPost.append("sub_titulo", document.querySelector("#postSubTitle").value);
+      frmPost.append("rutImagen", document.querySelector("#postImage").files[0]); // Asegúrate de que image es un input de tipo "file"
+      frmPost.append("categoria", document.querySelector("#categories").value);
+      frmPost.append("contenido_publicacion", document.querySelector("#postContent").value);
+      fetch(api + "actualizarPorId/" + idPost, {
+        method: "PUT",
+        body: frmPost
+      })
+        .then((res) => res.json())
+        .then((res) => {
+          console.log(res);
+          if (res.estado === true) {
+            Swal.fire({
+              position: "top",
+              title: "Se Modifico el post!",
+              icon: "success",
+              text: res.mensaje,
+              showConfirmButton: false,
+              timer: 1500
+            });
+          } else {
+            Swal.fire({
+              title: "Error!",
+              icon: "error",
+              text: res.mensaje
+            });
+          }
+        })
+        .catch((error) => {
+          Swal.fire({
+            title: "El servidor no responde!",
+            icon: "error",
+            text: error
+          });
+        });
 
-  // document.getElementById("frmPost").reset();
+      break;
+
+    default:
+      Swal.fire({
+        title: "Accion no valida",
+        icon: "error",
+        text: error
+      });
+      break;
+  }
+  document.getElementById("frmPost").reset();
   bootstrap.Modal.getInstance(document.getElementById("addPostModal")).hide();
 });
